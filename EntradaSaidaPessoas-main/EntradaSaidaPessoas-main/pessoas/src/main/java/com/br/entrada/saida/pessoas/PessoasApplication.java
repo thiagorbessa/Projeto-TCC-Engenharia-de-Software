2d60044f64e4b5dfafc8a70ef3887c368c85f2ca
thiagorbessa <43521019+thiagorbessa@users.noreply.github.com>
2026-01-26 09:17:17 -0300

    Alteração para garantir que o java rode no horário de Brasilia , sem importar o ambiente

--- EntradaSaidaPessoas-main/EntradaSaidaPessoas-main/pessoas/src/main/java/com/br/entrada/saida/pessoas/PessoasApplication.java
+++ EntradaSaidaPessoas-main/EntradaSaidaPessoas-main/pessoas/src/main/java/com/br/entrada/saida/pessoas/PessoasApplication.java
@@ -1,13 +1,22 @@
 package com.br.entrada.saida.pessoas;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class PessoasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PessoasApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		// Força o Java a usar o fuso horário de Brasília, não importa onde esteja rodando
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 	}
 
 }
